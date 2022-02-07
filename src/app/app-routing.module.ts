c2d3023729280be24d75edd7b34ebaea13f331f2
Rafal Zelazko <rzelazko@gmail.com>
2022-02-07 19:14:55 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AddEventComponent } from './epilepsy/add-event/add-event.component';
+import { AddMedicamentComponent } from './epilepsy/add-medicament/add-medicament.component';
+import { AddSeizureComponent } from './epilepsy/add-seizure/add-seizure.component';
+import { HomeComponent } from './home/home.component';
+import { CalendarComponent } from './reports/calendar/calendar.component';
+import { ChartsComponent } from './reports/charts/charts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'reports/charts', component: ChartsComponent },
+  { path: 'reports/calendar', component: CalendarComponent },
+  { path: 'add-seizure', component: AddSeizureComponent },
+  { path: 'add-medicament', component: AddMedicamentComponent },
+  { path: 'add-event', component: AddEventComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
