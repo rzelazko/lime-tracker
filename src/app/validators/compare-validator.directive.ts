57a4a052bab094d0c4837451c7af5bb040597279
Rafal Zelazko <rzelazko@gmail.com>
2022-02-09 19:28:01 +0100

    Compare validator use refernce to field instead of form control name

--- src/app/validators/compare-validator.directive.ts
+++ src/app/validators/compare-validator.directive.ts
@@ -1,38 +1,51 @@
-import { Attribute, Directive, ElementRef } from '@angular/core';
-import { AbstractControl, FormControl, NG_VALIDATORS, Validator } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Directive, forwardRef, Input, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  NgModel,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+} from '@angular/forms';
+import { Subscription, take } from 'rxjs';
 
 @Directive({
   selector: '[compareValidator]',
   providers: [
     {
       provide: NG_VALIDATORS,
-      useClass: CompareValidatorDirective,
+      useExisting: forwardRef(() => CompareValidatorDirective),
       multi: true,
     },
   ],
 })
 export class CompareValidatorDirective implements Validator {
-  constructor(
-    @Attribute('compareValidator') public compareToControl: string
-  ) {}
+  @Input('compareValidator') compareToInput: NgModel | null = null;
 
-  validate(formControl: FormControl) {
-    const compareToInput = formControl.root.get(this.compareToControl);
-    const currentInput = formControl;
-
-    if (currentInput.value === null) {
-      return null;
-    }
-
-    if (compareToInput) {
-      const subscription: Subscription = compareToInput.valueChanges.subscribe(() => {
-        currentInput.updateValueAndValidity();
-        subscription.unsubscribe();
-      });
-    }
-    return compareToInput && compareToInput.value !== currentInput.value
-      ? { passwordMatchError: true }
-      : null;
+  validate(control: AbstractControl): ValidationErrors | null {
+      return CompareValidator(this.compareToInput)(control);
   }
+
 }
+
+export const CompareValidator =
+  (compareToInput: NgModel | null): ValidatorFn =>
+  (formControl: AbstractControl) => {
+    let result: ValidationErrors | null = null;
+    if (compareToInput) {
+      const currentInput = formControl;
+
+      if (currentInput.value !== null) {
+        const subscription: Subscription = compareToInput.control.valueChanges
+          .pipe(take(1))
+          .subscribe(() => {
+            currentInput.updateValueAndValidity();
+          });
+      }
+      result =
+        compareToInput.control.value !== currentInput.value
+          ? { passwordMatchError: true }
+          : null;
+    }
+    return result;
+  };
