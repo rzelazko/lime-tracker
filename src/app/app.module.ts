60f23ef9d7df36e554c50481184f99f7e22228a5
Rafal Zelazko <rzelazko@gmail.com>
2022-02-17 16:15:30 +0100

    Add doses tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -12,6 +11,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -62,7 +62,6 @@ import { CompareValidatorDirective } from './validators/compare-validator.direct
     // material
     MatButtonModule,
     MatCardModule,
-    MatChipsModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
@@ -70,6 +69,7 @@ import { CompareValidatorDirective } from './validators/compare-validator.direct
     MatSidenavModule,
     MatTabsModule,
     MatToolbarModule,
+    MatTooltipModule,
   ],
   exports: [CompareValidatorDirective],
   providers: [],
