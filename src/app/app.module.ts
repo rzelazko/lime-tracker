7884597632b1d517c4c7dafd60c221c89071d58a
Rafal Zelazko <rzelazko@gmail.com>
2022-04-26 17:53:52 +0200

    Add language switcher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -156,6 +157,7 @@ export const persistenceEnabled = new Promise<boolean>((resolve) => {
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatMenuModule,
     MatMomentDateModule,
     MatProgressSpinnerModule,
     MatSelectModule,
