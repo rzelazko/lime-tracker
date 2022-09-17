4e9f7049629926ceb566d5f8ff4b50ab1fe14e8c
Rafal Zelazko <rzelazko@gmail.com>
2022-09-17 17:05:01 +0200

    Show priods card only to female

--- src/app/epilepsy/pages/dashboard/dashboard.component.ts
+++ src/app/epilepsy/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Duration } from 'moment';
 import { catchError, Observable, throwError } from 'rxjs';
+import { UserData } from './../../../auth/models/user-details.model';
 import { Event } from './../../../shared/models/event.model';
 import { Medication } from './../../../shared/models/medication.model';
 import { Period } from './../../../shared/models/period.model';
 import { Seizure } from './../../../shared/models/seizure.model';
+import { AuthService } from './../../../shared/services/auth.service';
 import { DashboardService } from './../../../shared/services/dashboard.service';
+import { UserDetailsService } from './../../../shared/services/user-details.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,6 +16,7 @@ import { DashboardService } from './../../../shared/services/dashboard.service';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  userDetails$: Observable<UserData>;
   medications$: Observable<Medication[]>;
   lastSeizures$: Observable<Seizure[]>;
   lastEvents$: Observable<Event[]>;
@@ -23,25 +27,34 @@ export class DashboardComponent implements OnInit {
   eventsError?: string;
   periodsError?: string;
 
-  constructor(dashboardService: DashboardService) {
+  constructor(
+    dashboardService: DashboardService,
+    private auth: AuthService,
+    private userDetails: UserDetailsService
+  ) {
+    this.userDetails$ = userDetails.get(auth.user());
+
     this.medications$ = dashboardService.currentMedications().pipe(
       catchError((error) => {
         this.seizureError = error;
         return throwError(() => error);
       })
     );
+
     this.lastSeizures$ = dashboardService.lastSeizures().pipe(
       catchError((error) => {
         this.medicationsError = error;
         return throwError(() => error);
       })
     );
+
     this.lastEvents$ = dashboardService.lastEvents().pipe(
       catchError((error) => {
         this.eventsError = error;
         return throwError(() => error);
       })
     );
+
     this.lastPeriods$ = dashboardService.lastPeriods().pipe(
       catchError((error) => {
         this.periodsError = error;
