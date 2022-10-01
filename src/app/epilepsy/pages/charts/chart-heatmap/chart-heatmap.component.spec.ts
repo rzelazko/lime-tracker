b0500fce0d39faffa82333aab2ad53483d0fdb5e
Rafal Zelazko <rzelazko@gmail.com>
2022-10-01 11:04:10 +0200

    Test charts

--- src/app/epilepsy/pages/charts/chart-heatmap/chart-heatmap.component.spec.ts
+++ src/app/epilepsy/pages/charts/chart-heatmap/chart-heatmap.component.spec.ts
@@ -1,12 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressSpinner, MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartComponent, NgApexchartsModule } from 'ng-apexcharts';
+import { delay, of, throwError } from 'rxjs';
+import { ErrorCardComponent } from './../../../../shared/error-card/error-card.component';
+import { ChartData } from './../../../../shared/models/chart-data.model';
 import { ChartHeatmapService } from './../../../../shared/services/chart-heatmap.service';
 import { ChartHeatmapComponent } from './chart-heatmap.component';
 
@@ -16,12 +21,17 @@ describe('ChartHeatmapComponent', () => {
   let chartServiceSpy: jasmine.SpyObj<ChartHeatmapService>;
 
   beforeEach(async () => {
-    const chartServiceSpyObj = jasmine.createSpyObj('ChartHeatmapService', ['seizureSerie']);
+    const chartServiceSpyObj = jasmine.createSpyObj('ChartHeatmapService', [
+      'setYear',
+      'subtitle',
+      'seizureSerie',
+    ]);
 
     await TestBed.configureTestingModule({
-      declarations: [ChartHeatmapComponent],
+      declarations: [ChartHeatmapComponent, ErrorCardComponent],
       providers: [{ provide: ChartHeatmapService, useValue: chartServiceSpyObj }],
       imports: [
+        NgApexchartsModule,
         NoopAnimationsModule,
         MatCardModule,
         MatIconModule,
@@ -45,5 +55,66 @@ describe('ChartHeatmapComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // TODO add tests
+  it('should show progress spinner on load', fakeAsync(() => {
+    // given
+    const chartData: ChartData[] = [];
+    chartServiceSpy.seizureSerie.and.returnValue(of(chartData).pipe(delay(100)));
+
+    // when
+    component.ngOnChanges({});
+    fixture.detectChanges();
+
+    // then
+    expect(fixture.debugElement.queryAll(By.directive(MatProgressSpinner)).length).toBe(1);
+    component.ngOnChanges({});
+    tick(100);
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.directive(MatProgressSpinner)).length).toBe(0);
+  }));
+
+  it('should show chart when seizureSerie ready', async () => {
+    // given
+    const chartData: ChartData[] = [];
+    chartServiceSpy.seizureSerie.and.returnValue(of(chartData));
+
+    // when
+    component.ngOnChanges({});
+    fixture.detectChanges();
+
+    // then
+    expect(fixture.debugElement.queryAll(By.directive(MatProgressSpinner)).length).toBe(0);
+    expect(fixture.debugElement.queryAll(By.directive(ChartComponent)).length).toBe(1);
+  });
+
+  it('should show error from Error object', () => {
+    // given
+    chartServiceSpy.seizureSerie.and.returnValue(throwError(() => new Error('test message')));
+
+    // when
+    component.ngOnChanges({});
+    fixture.detectChanges();
+
+    // then
+    expect(fixture.debugElement.queryAll(By.directive(MatProgressSpinner)).length).toBe(0);
+    expect(fixture.debugElement.queryAll(By.directive(ErrorCardComponent)).length).toBe(1);
+    expect(
+      fixture.debugElement.query(By.directive(ErrorCardComponent)).nativeElement.textContent
+    ).toContain('Error: test message');
+  });
+
+  it('should show error from string', () => {
+    // given
+    chartServiceSpy.seizureSerie.and.returnValue(throwError(() => `test message`));
+
+    // when
+    component.ngOnChanges({});
+    fixture.detectChanges();
+
+    // then
+    expect(fixture.debugElement.queryAll(By.directive(MatProgressSpinner)).length).toBe(0);
+    expect(fixture.debugElement.queryAll(By.directive(ErrorCardComponent)).length).toBe(1);
+    expect(
+      fixture.debugElement.query(By.directive(ErrorCardComponent)).nativeElement.textContent
+    ).toContain('Error: test message');
+  });
 });
--- src/app/shared/services/chart-heatmap.service.spec.ts
+++ src/app/shared/services/chart-heatmap.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import * as Moment from 'moment';
 import { extendMoment } from 'moment-range';
+import { of } from 'rxjs';
+import { ChartData } from '../models/chart-data.model';
+import { Seizure } from '../models/seizure.model';
 import { ChartHeatmapService } from './chart-heatmap.service';
 import { SeizuresService } from './seizures.service';
 
@@ -37,5 +40,134 @@ describe('ChartHeatmapService', () => {
     expect(service).toBeTruthy();
   });
 
-  // TODO add tests
+  it('should have valid amount of series and days for year with 365 days', async () => {
+    // given
+    service.setYear(2019);
+    const seizuresList: Seizure[] = [];
+    seizuresServiceSpy.listCollection.and.returnValue(of(seizuresList));
+
+    // when
+    const seizuresSerie$ = service.seizureSerie();
+
+    // then
+    seizuresSerie$.subscribe((seizuresSerie: ChartData[]) => {
+      expect(seizuresSerie.length).toBe(5);
+      for (let i = 0; i < seizuresSerie.length; i++) {
+        expect(seizuresSerie[i].data.length).toBe(73);
+      }
+    });
+  });
+
+  it('should have valid amount of series and days for curent year', async () => {
+    // given
+    // curent year set in beforeEach
+    const seizuresList: Seizure[] = [];
+    seizuresServiceSpy.listCollection.and.returnValue(of(seizuresList));
+
+    // when
+    const seizuresSerie$ = service.seizureSerie();
+
+    // then
+    seizuresSerie$.subscribe((seizuresSerie: ChartData[]) => {
+      expect(seizuresSerie.length).toBe(5);
+      for (let i = 0; i < seizuresSerie.length; i++) {
+        expect(seizuresSerie[i].data.length).toBe(73);
+      }
+    });
+  });
+
+  it('should have valid amount of series and days for year with 366 days', async () => {
+    // given
+    service.setYear(2020);
+    const seizuresList: Seizure[] = [];
+    seizuresServiceSpy.listCollection.and.returnValue(of(seizuresList));
+
+    // when
+    const seizuresSerie$ = service.seizureSerie();
+
+    // then
+    seizuresSerie$.subscribe((seizuresSerie: ChartData[]) => {
+      expect(seizuresSerie.length).toBe(6);
+      for (let i = 0; i < seizuresSerie.length; i++) {
+        expect(seizuresSerie[i].data.length).toBe(61);
+      }
+    });
+  });
+
+  it('should have valid values', async () => {
+    // given
+    service.setYear(2019);
+    const seizuresList: Seizure[] = [
+      {
+        objectType: 'SEIZURE',
+        id: 's1',
+        occurred: moment('2019-01-01 00:00:00'),
+        duration: moment.duration(5, 'minutes'),
+        type: 'some type',
+      },
+      {
+        objectType: 'SEIZURE',
+        id: 's2',
+        occurred: moment('2019-01-01 00:00:01'),
+        duration: moment.duration(5, 'minutes'),
+        type: 'some type',
+      },
+      {
+        objectType: 'SEIZURE',
+        id: 's3',
+        occurred: moment('2019-01-01 23:59:59'),
+        duration: moment.duration(5, 'minutes'),
+        type: 'some type',
+      },
+      {
+        objectType: 'SEIZURE',
+        id: 's4',
+        occurred: moment('2019-07-12 12:00:00'),
+        duration: moment.duration(5, 'minutes'),
+        type: 'some type',
+      },
+      {
+        objectType: 'SEIZURE',
+        id: 's5',
+        occurred: moment('2019-12-31 01:00:00'),
+        duration: moment.duration(5, 'minutes'),
+        type: 'some type',
+      },
+      {
+        objectType: 'SEIZURE',
+        id: 's6',
+        occurred: moment('2019-12-31 10:00:00'),
+        duration: moment.duration(5, 'minutes'),
+        type: 'some type',
+      },
+    ];
+    seizuresServiceSpy.listCollection.and.returnValue(of(seizuresList));
+
+    // when
+    const seizuresSerie$ = service.seizureSerie();
+
+    // then
+    seizuresSerie$.subscribe((seizuresSerie: ChartData[]) => {
+      let totalVerfiedDays = 0;
+      const label0101 = moment('2019-01-01').format('LL');
+      const label0712 = moment('2019-07-12').format('LL');
+      const label1231 = moment('2019-12-31').format('LL');
+      for (let i = 0; i < seizuresSerie.length; i++) {
+        for (let j = 0; j < seizuresSerie[i].data.length; j++) {
+          const data = seizuresSerie[i].data[j];
+          if (data.label === label0101) {
+            expect(data.y).toBe(3);
+          } else if (data.label === label0712) {
+            expect(data.y).toBe(1);
+          } else if (data.label === label1231) {
+            expect(data.y).toBe(2);
+          } else {
+            expect(data.y).toBe(0);
+          }
+          totalVerfiedDays++;
+        }
+      }
+      expect(totalVerfiedDays).toBe(365);
+    });
+  });
 });
