b866fa9c8c91a0ce1768a11b39f4c40637e86c6c
Rafal Zelazko <rzelazko@gmail.com>
2022-10-07 10:55:21 +0200

    Chart summary - route observer instead of onChange

--- src/app/epilepsy/pages/charts/chart-summary/chart-summary.component.ts
+++ src/app/epilepsy/pages/charts/chart-summary/chart-summary.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ApexAxisChartSeries, ApexYAxis } from 'ng-apexcharts';
-import { combineLatest, map, Subscription } from 'rxjs';
+import { combineLatest, map, Observable, of } from 'rxjs';
+import { catchError, ignoreElements, switchMap } from 'rxjs/operators';
 import { ChartData } from './../../../../shared/models/chart-data.model';
 import { ChartOptions } from './../../../../shared/models/chart-options.model';
 import { ChartSummaryService } from './../../../../shared/services/chart-summary.service';
@@ -10,109 +12,104 @@ import { ChartSummaryService } from './../../../../shared/services/chart-summary
   templateUrl: './chart-summary.component.html',
   styleUrls: ['./chart-summary.component.scss'],
 })
-export class ChartSummaryComponent implements OnInit, OnDestroy, OnChanges {
-  @Input() selectedYear?: number;
+export class ChartSummaryComponent implements OnInit {
   @Input() titleOffset: number = 0;
   @Input() height: number = 100;
-  error?: string;
-  summaryChart?: ChartOptions;
-  subsription?: Subscription;
-  medicationsData?: ChartData[];
-  eventsData?: ChartData;
-  seizuresData?: ChartData;
+  chartOptions$?: Observable<ChartOptions>;
+  chartError$?: Observable<String>;
 
-  constructor(private chartService: ChartSummaryService) {}
+  constructor(private chartService: ChartSummaryService, private activatedRoute: ActivatedRoute) {}
 
-  ngOnChanges(_changes: SimpleChanges): void {
-    this.summaryChart = undefined;
-    this.chartService.setYear(this.selectedYear);
+  ngOnInit(): void {
+    this.chartOptions$ = this.activatedRoute.params.pipe(
+      map((routeParams): number | undefined => routeParams['year']),
+      switchMap((selectedYear) => {
+        this.chartService.setYear(selectedYear);
+        return combineLatest([
+          this.chartService.medicationsSeries(),
+          this.chartService
+            .eventsSerie()
+            .pipe(map((data) => ({ name: $localize`:@@title-events:Events`, ...data }))),
+          this.chartService
+            .seizureSerie()
+            .pipe(map((data) => ({ name: $localize`:@@title-seizures:Seizures`, ...data }))),
+        ]);
+      }),
+      map(([medicationsData, eventsData, seizuresData]) =>
+        this.toChartOptions(medicationsData, eventsData, seizuresData)
+      )
+    );
 
-    this.subsription?.unsubscribe();
-    this.subsription = combineLatest([
-      this.chartService.medicationsSeries().pipe(map((data) => (this.medicationsData = data))),
-      this.chartService.eventsSerie().pipe(
-        map((data) => ({ name: $localize`:@@title-events:Events`, ...data })),
-        map((data) => (this.eventsData = data))
-      ),
-      this.chartService.seizureSerie().pipe(
-        map((data) => ({ name: $localize`:@@title-seizures:Seizures`, ...data })),
-        map((data) => (this.seizuresData = data))
-      ),
-    ]).subscribe({
-      next: () => this.updateChart(),
-      error: (error) => (this.error = $localize`:@@error-message:Error: ${error.message || error}`),
-    });
+    this.chartError$ = this.chartOptions$.pipe(
+      ignoreElements(),
+      catchError((error) => of($localize`:@@error-message:Error: ${error.message || error}`))
+    );
   }
 
-  ngOnInit(): void {}
-
-  ngOnDestroy(): void {
-    this.subsription?.unsubscribe();
-  }
-
-  private updateChart() {
-    const labelFormatter = (value: number, opts: { dataPointIndex: number }, labels?: string[]) => {
-      if (labels && labels[opts?.dataPointIndex]) {
-        return labels[opts.dataPointIndex];
-      }
-      return value.toFixed(0);
-    };
-
+  private toChartOptions = (
+    medicationsData: ChartData[],
+    eventsData: ChartData,
+    seizuresData: ChartData
+  ): ChartOptions => {
     const yaxis: ApexYAxis[] = [];
-    if (this.seizuresData && this.seizuresData.data.length > 0) {
+    const series: ApexAxisChartSeries = [];
+
+    if (seizuresData && seizuresData.data.length > 0) {
       yaxis.push({
         opposite: false,
         axisTicks: { show: true },
         axisBorder: { show: true },
-        seriesName: this.seizuresData.name,
+        seriesName: seizuresData.name,
         title: {
-          text: this.seizuresData.name,
+          text: seizuresData.name,
         },
+      });
+      series.push({
+        name: seizuresData.name,
+        type: 'column',
+        data: seizuresData.data,
       });
     }
-    this.medicationsData?.forEach((medicationData, i) => {
-      yaxis.push({
-        show: i < 1,
-        opposite: true,
-        axisTicks: { show: true },
-        axisBorder: { show: true },
-        seriesName: this.medicationsData ? this.medicationsData[0].name : medicationData.name,
-        title: {
-          text: $localize`:@@chart-summary-med-per-day:Medication (per day)`,
-        },
-        labels: {
-          formatter: (value: number, opts: { dataPointIndex: number }) =>
-            labelFormatter(value, opts, medicationData.labels),
-        },
-      });
-    });
-    if (this.eventsData && this.eventsData.data.length > 0) {
+
+    if (eventsData && eventsData.data.length > 0) {
       yaxis.push({
         show: false,
         axisTicks: { show: false },
         axisBorder: { show: false },
-        seriesName: this.eventsData.name,
+        seriesName: eventsData.name,
         title: {
-          text: this.eventsData.name,
+          text: eventsData.name,
         },
         min: 0,
         tickAmount: 2,
         labels: {
           formatter: (value: number, opts: { dataPointIndex: number }) =>
-            labelFormatter(value, opts, this.eventsData?.labels),
+            this.labelFormatter(value, opts, eventsData?.labels),
         },
+      });
+      series.push({
+        name: eventsData.name,
+        type: 'scatter',
+        data: eventsData.data,
       });
     }
 
-    const series: ApexAxisChartSeries = [];
-    if (this.seizuresData && this.seizuresData.data.length > 0) {
-      series.push({
-        name: this.seizuresData.name,
-        type: 'column',
-        data: this.seizuresData.data,
+    medicationsData.forEach((medicationData, i) => {
+      yaxis.push({
+        show: i < 1,
+        opposite: true,
+        axisTicks: { show: true },
+        axisBorder: { show: true },
+        seriesName: medicationsData ? medicationsData[0].name : medicationData.name,
+        title: {
+          text: $localize`:@@chart-summary-med-per-day:Medication (per day)`,
+        },
+        labels: {
+          formatter: (value: number, opts: { dataPointIndex: number }) =>
+            this.labelFormatter(value, opts, medicationData.labels),
+        },
       });
-    }
-    this.medicationsData?.forEach((medicationData) => {
+
       series.push({
         name: medicationData.name,
         type: 'line',
@@ -120,16 +117,8 @@ export class ChartSummaryComponent implements OnInit, OnDestroy, OnChanges {
       });
     });
 
-    if (this.eventsData && this.eventsData.data.length > 0) {
-      series.push({
-        name: this.eventsData.name,
-        type: 'scatter',
-        data: this.eventsData.data,
-      });
-    }
-
-    this.summaryChart = {
-      chart: { height: this.height, type: 'line' },
+    return {
+      component: { height: this.height, type: 'line' },
       dataLabels: { enabled: false },
       stroke: { width: [1, 1, 4] },
       title: {
@@ -150,5 +139,12 @@ export class ChartSummaryComponent implements OnInit, OnDestroy, OnChanges {
       yaxis,
       series,
     };
-  }
+  };
+
+  private labelFormatter = (value: number, opts: { dataPointIndex: number }, labels?: string[]) => {
+    if (labels && labels[opts?.dataPointIndex]) {
+      return labels[opts.dataPointIndex];
+    }
+    return value.toFixed(0);
+  };
 }
