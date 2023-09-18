55d18856e7c91b7adff7a3ff5bd571f4088e09a7
Rafal Zelazko <rzelazko@gmail.com>
2023-09-18 18:12:15 +0200

    Upgrade Angular to version 16

--- src/app/shared/guards/year-in-range.guard.ts
+++ src/app/shared/guards/year-in-range.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import * as moment from 'moment';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class YearInRangeGuard implements CanActivate {
+export class YearInRangeGuard  {
   constructor(private router: Router) {}
 
   canActivate(
