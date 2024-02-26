b63571d483a5d2b8524fc94f4b1b54a2882f3b66
Rafal Zelazko <rzelazko@gmail.com>
2024-02-26 20:13:02 +0100

    Copy default language to public folder

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,49 +1,73 @@
-const fs = require('fs');
-const path = require('path');
-const xml2js = require('xml2js');
+const fs = require("fs-extra");
+const path = require("path");
+const xml2js = require("xml2js");
 
-const packageJson = require(path.join(__dirname, '..', 'package.json'));
-const appName = packageJson.name;
+const translateManifests = async (sourceDirectory, destinationDirectory) => {
+  const xliffFiles = fs
+    .readdirSync(path.join(sourceDirectory, "locale"))
+    .map((xlfPath) => path.join(sourceDirectory, "locale", xlfPath));
+  const manifestTemplatePath = path.join(sourceDirectory, "manifest.webmanifest");
 
-const sourceDirectory = path.join(__dirname, '..', 'src');
-const destinationDirectory = path.join(__dirname, '..', 'dist', appName);
+  function nameToCamelCase(name) {
+    const parts = name.split("-");
+    parts[0] = parts[0].toLowerCase();
+    const uppercased = parts.slice(1).map((part) => {
+      return part.charAt(0).toUpperCase() + part.slice(1);
+    });
 
-const xliffFiles = fs.readdirSync(path.join(sourceDirectory, 'locale'))
-  .map(xlfPath => path.join(sourceDirectory, 'locale', xlfPath));
-const manifestTemplatePath = path.join(sourceDirectory, 'manifest.webmanifest');
+    return [parts[0], ...uppercased].join("");
+  }
 
-function nameToCamelCase(name) {
-  const parts = name.split('-');
-  parts[0] = parts[0].toLowerCase();
-  const uppercased = parts.slice(1).map(part => {
-    return part.charAt(0).toUpperCase() + part.slice(1);
+  for (const xlfPath of xliffFiles) {
+    console.log("Processing Xliff file:", xlfPath);
+    const xliffData = await xml2js.parseStringPromise(fs.readFileSync(xlfPath), {
+      trim: true,
+      explicitArray: false,
+      attrNameProcessors: [nameToCamelCase],
+      tagNameProcessors: [nameToCamelCase],
+    });
+    const xliffFile = xliffData.xliff.file;
+
+    const sourceLanguage = xliffFile.$.sourceLanguage;
+    const targetLanguage = xliffFile.$.targetLanguage;
+
+    const language = targetLanguage || sourceLanguage;
+
+    const manifestTemplate = fs.readFileSync(manifestTemplatePath, "utf8");
+    const manifestOutput = manifestTemplate.replace(/"@@(.+)"/g, (match, key) => {
+      const transUnit = xliffFile.body.transUnit.find((tu) => tu.$.id === key);
+      return JSON.stringify(transUnit.target || transUnit.source);
+    });
+
+    const manifestDestinationPath = path.join(
+      destinationDirectory,
+      language,
+      "manifest.webmanifest"
+    );
+    console.log("Writing manifest file:", manifestDestinationPath);
+    fs.writeFileSync(manifestDestinationPath, manifestOutput, "utf8");
+  }
+};
+
+const copyDefaultLang = (destinationDirectory) => {
+  console.log("Copying default language to root directory");
+
+  const defaultLanguageDirectory = path.join(destinationDirectory, "en");
+  fs.copySync(defaultLanguageDirectory, destinationDirectory, {
+    overwrite: true,
+    errorOnExist: false,
   });
+};
 
-  return [parts[0], ...uppercased].join('');
-}
+const main = async () => {
+  const packageJson = require(path.join(__dirname, "..", "package.json"));
+  const appName = packageJson.name;
 
-xliffFiles.forEach(async xlfPath => {
-  console.log('Processing Xliff file:', xlfPath);
-  const xliffData = await xml2js.parseStringPromise(fs.readFileSync(xlfPath), {
-    trim: true,
-    explicitArray: false,
-    attrNameProcessors: [nameToCamelCase],
-    tagNameProcessors: [nameToCamelCase]
-  });
-  const xliffFile = xliffData.xliff.file;
+  const sourceDirectory = path.join(__dirname, "..", "src");
+  const destinationDirectory = path.join(__dirname, "..", "dist", appName);
 
-  const sourceLanguage = xliffFile.$.sourceLanguage;
-  const targetLanguage = xliffFile.$.targetLanguage;
+  await translateManifests(sourceDirectory, destinationDirectory);
+  copyDefaultLang(destinationDirectory);
+};
 
-  const language = targetLanguage || sourceLanguage;
-
-  const manifestTemplate = fs.readFileSync(manifestTemplatePath, 'utf8');
-  const manifestOutput = manifestTemplate.replace(/"@@(.+)"/g, (match, key) => {
-    const transUnit = xliffFile.body.transUnit.find(tu => tu.$.id === key);
-    return JSON.stringify(transUnit.target || transUnit.source);
-  });
-
-  const manifestDestinationPath = path.join(destinationDirectory, language, 'manifest.webmanifest');
-  console.log('Writing manifest file:', manifestDestinationPath);
-  fs.writeFileSync(manifestDestinationPath, manifestOutput, 'utf8');
-});
+main();
