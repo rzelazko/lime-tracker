cab00b26cb93c3734ee1c45054748c261f95da53
Rafal Zelazko <rzelazko@gmail.com>
2026-02-14 16:18:31 +0100

    Reinstate Firebase SDK initialization in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// Ensure Firebase SDK is initialized before Angular bootstraps.
+import './app/shared/firebase';
 
 if (environment.production) {
   enableProdMode();
